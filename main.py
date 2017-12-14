afdc7826378ce6e88bad2f79a134c0b8384a0000
Terri Riggle <codeofarmour@outlook.com>
2017-12-13 21:32:08 -0500

    added folder for flicklist-flask

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) # app is the object, __name__ is the placeholder; whatever's in @app.route("/") will be passed to (__name__)
 app.config['DEBUG'] = True
 
-@app.route("/")
-def index():
+@app.route("/") # decorator/notation - tells application that this is the route
+def index(): # named "index" bc it's the name of the page we're displaying, e.g. index.html. This tells app to go to decorator route
     return "Hello World"
-
+'''
+@app.route("/about")
+def about():
+    return "World"
+'''
 app.run()
